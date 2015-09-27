874ecfca76866f03c4931bd01f5ce84bd55596cd
Austin Williams <Austin@Austins-MacBook-Pro.local>
2015-09-26 18:04:05 -0700

    basic blog is working!

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -19,6 +19,8 @@ from django.conf import settings
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MAIN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -41,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core', 
 )
 
 MIDDLEWARE_CLASSES = (
@@ -77,17 +80,19 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if ON_HEROKU == True :
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
+else:    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -120,3 +125,12 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = 'staticfiles'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_FORCE_HTTP_URL = True
+AWS_QUERYSTRING_AUTH = False
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWSSecretKey')
+AWS_ACCESS_KEY_ID = os.environ.get('AWSAccessKeyId')
+
+AWS_STORAGE_BUCKET_NAME = 'coffeedapponemonth2'
+
--- core/urls.py
+++ core/urls.py
@@ -18,4 +18,6 @@ import core.views as coreviews
 
 urlpatterns = [
     url(r'^$', coreviews.LandingView.as_view()),
+    url(r'location/$', coreviews.LocationListView.as_view()),
+    url(r'location/(?P<pk>\d+)/detail/$', coreviews.LocationDetailView.as_view(), name="location_list"),
 ]
